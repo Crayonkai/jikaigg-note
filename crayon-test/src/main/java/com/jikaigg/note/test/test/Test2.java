b964891d37abb7bb76cd6060971bab8221078475
Crayonkai <jikaigg@gmail.com>
2022-08-19 18:15:03 +0800

    add

---
+++ crayon-test/src/main/java/com/jikaigg/note/test/test/Test2.java
@@ -0,0 +1,10 @@
+package com.jikaigg.note.test.test;
+
+public class Test2 {
+    public static void main(String[] args) {
+        String str1 = "柳柴";
+        String str2 = "柴柕";
+        System.out.println("str1 hashcode : " + str1.hashCode());
+        System.out.println("str2 hashcode : " + str2.hashCode());
+    }
+}
