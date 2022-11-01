35b08fa615bfbd4f775db25f31666389927c6af0
jikaigg <jikaigg@gmail.com>
2022-11-02 00:36:44 +0800

    transaction test

---
+++ crayon-test/src/main/java/com/jikaigg/note/algorithm/base/Base20.java
@@ -0,0 +1,8 @@
+package com.jikaigg.note.algorithm.base;
+
+public class Base20 {
+    public static void main(String[] args) {
+        String s1 = "123456";
+        System.out.println(s1.length());
+    }
+}
